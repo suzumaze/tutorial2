0cbf34fa8f843a5d0fb1d0fe08e709fea8ba1af0
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-14 17:19:59 +0900

    add resource

---
+++ src/Resource/App/Tickets.php
@@ -0,0 +1,22 @@
+<?php
+namespace MyVendor\Ticket\Resource\App;
+
+use BEAR\RepositoryModule\Annotation\Cacheable;
+use BEAR\Resource\Annotation\JsonSchema;
+use BEAR\Resource\ResourceObject;
+use Ray\Query\Annotation\AliasQuery;
+
+/**
+ * @Cacheable
+ */
+class Tickets extends ResourceObject
+{
+    /**
+     * @JsonSchema(schema="tickets.json")
+     * @AliasQuery("ticket_list")
+     */
+    public function onGet() : ResourceObject
+    {
+        return $this;
+    }
+}
