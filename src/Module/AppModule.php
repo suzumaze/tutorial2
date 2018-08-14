0e672bcc8cd80e953e4fb99acac11116739f5b9a
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-14 17:06:58 +0900

    install module

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -2,8 +2,12 @@
 namespace MyVendor\Ticket\Module;
 
 use BEAR\Package\PackageModule;
+use BEAR\Resource\Module\JsonSchemaModule;
 use josegonzalez\Dotenv\Loader;
+use Ray\AuraSqlModule\AuraSqlModule;
 use Ray\Di\AbstractModule;
+use Ray\IdentityValueModule\IdentityValueModule;
+use Ray\Query\SqlQueryModule;
 
 class AppModule extends AbstractModule
 {
@@ -14,6 +18,10 @@ class AppModule extends AbstractModule
     {
         $appDir = dirname(__DIR__, 2);
         (new Loader($appDir . '/.env'))->parse()->toEnv(true);
+        $this->install(new AuraSqlModule($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_SLAVE']));
+        $this->install(new SqlQueryModule($appDir . '/var/sql'));
+        $this->install(new IdentityValueModule);
+        $this->install(new JsonSchemaModule($appDir . '/var/json_schema', $appDir . '/var/json_validate'));
         $this->install(new PackageModule);
     }
 }
