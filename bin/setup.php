30c06943c30bd5f216430bdf1c31ff36b7fb1b7d
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-14 17:07:16 +0900

    db setup script

--- bin/setup.php
+++ bin/setup.php
@@ -1,6 +1,13 @@
 <?php
-
+require dirname(__DIR__) . '/vendor/autoload.php';
+// dir
 chdir(dirname(__DIR__));
 passthru('rm -rf var/tmp/*');
 passthru('chmod 775 var/tmp');
 passthru('chmod 775 var/log');
+// db
+(new josegonzalez\Dotenv\Loader(dirname(__DIR__) . '/.env'))->parse()->toEnv();
+$db = new PDO('mysql:', $_ENV['DB_USER'], $_ENV['DB_PASS']);
+$db->exec('CREATE DATABASE IF NOT EXISTS ' . $_ENV['DB_NAME']);
+$db->exec('CREATE DATABASE IF NOT EXISTS ' . $_ENV['DB_NAME'] . '_test');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
