78fa4e2487c89cbccea21fb357560043aa800bc3
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-23 01:19:54 +0900

    add /index curie

---
+++ src/Resource/App/Index.php
@@ -0,0 +1,35 @@
+<?php
+namespace MyVendor\Ticket\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+
+class Index extends ResourceObject
+{
+    public $body = [
+        'message' => 'Welcome to the MyVendor.Ticket API !',
+        '_links' => [
+            'self' => [
+                'href' => '/',
+            ],
+            'curies' => [
+                'href' => 'http://localhost:8081/rels/{?rel}',
+                'name' => 'kt',
+                'templated' => true
+            ],
+            'kt:ticket' => [
+                'href' => '/ticket',
+                'title' => 'tickets item',
+                'templated' => true
+            ],
+            'kt:tickets' => [
+                'href' => '/tickets',
+                'title' => 'ticket list'
+            ]
+        ]
+    ];
+
+    public function onGet()
+    {
+        return $this;
+    }
+}
